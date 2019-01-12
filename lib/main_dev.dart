ab5f1cbbdb7fd4500f91aaab7e420d21de62fb8f
Lukasz Kujawiak <fcjuventus@poczta.onet.pl>
2019-01-12 23:59:23 +0100

    Added and tested remote dev tools custom action decoder. It's commented out ang can be used for debugging in future

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:redux_dev_tools/redux_dev_tools.dart';
 import 'package:redux_epics/redux_epics.dart';
+import 'package:redux_remote_devtools/redux_remote_devtools.dart';
 
 import './app.dart';
 import './reducer.dart';
+import './spot/spot.action.dart';
 import './state.dart';
 import 'navigation/navigation_launcher/navigation_launcher.epics.dart';
 
 void main() async {
   final epicMiddleware = EpicMiddleware(launchNavigationEpic);
 
-  // final remoteDevtoolsMiddleware = RemoteDevToolsMiddleware('192.168.1.176:8000');
+  // final remoteDevtoolsMiddleware = RemoteDevToolsMiddleware(
+  // '192.168.1.176:8000',
+  // actionDecoder: Decoder(),
+  // );
   // await remoteDevtoolsMiddleware.connect();
 
   final store = DevToolsStore<AppState>(
@@ -27,4 +32,13 @@ void main() async {
   runApp(App(
     store: store,
   ));
+}
+
+class Decoder implements ActionDecoder {
+  @override
+  decode(payload) {
+    final map = payload as Map<String, dynamic>;
+    if (map['type'] == 'SelectSpotAction')
+      return SelectSpotAction.fromJson(map);
+  }
 }
