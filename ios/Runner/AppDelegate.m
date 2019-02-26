b4ee2632e0e0bd5a974f37a72a7d16f33972445a
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2019-02-26 23:56:22 +0100

    Upgraded iOS and Android projects to the latest Flutter templates structure

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,11 +1,10 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GMSServices provideAPIKey:@"AIzaSyBBQ5oenI6aka1sYqShFyS25-B7LCJ8sk4"];
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
