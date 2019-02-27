919b260b7a1bfbdcc1688c1af2c6125c37e7a0fd
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2019-02-27 22:00:13 +0100

    Added build name and number in ci build.sh
    Fixes #5

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-flutter build apk
+TIMESTAMP=$(($(date +%s)/10))
+flutter build apk --build-name=0.0.1 --build-number=$TIMESTAMP
 cd android
 bundle install
 fastlane android_appcenter_upload
